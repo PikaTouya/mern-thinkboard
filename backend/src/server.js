e87df38fc739600f849f20d379f6bf2e9f20be1f
Erwin <erwinsiatama.es@gmail.com>
2025-08-01 23:18:29 +0700

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express"; // if you want to use import sintax in  node js, u should add "type" : "module" in package.json
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoute from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
@@ -14,12 +15,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 //middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
@@ -32,6 +37,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
